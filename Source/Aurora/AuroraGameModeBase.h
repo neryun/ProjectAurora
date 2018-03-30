9c1b3bd651fc7cd491b2b26e16f8c1d8c2714e78
MARTIN-PC\neryu <neryun@gmail.com>
2018-03-30 16:47:16 +0200

    Init

---
+++ Source/Aurora/AuroraGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuroraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURORA_API AAuroraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
