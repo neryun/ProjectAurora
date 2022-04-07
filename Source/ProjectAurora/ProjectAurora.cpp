907a4281ceb5e47ce02688be313e7e9cb7ac2bdd
neryun <neryun@gmail.com>
2022-04-07 21:21:57 +0200

    Init

---
+++ Source/ProjectAurora/ProjectAurora.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectAurora.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectAurora, "ProjectAurora" );
+
+DEFINE_LOG_CATEGORY(LogProjectAurora)
+ 
